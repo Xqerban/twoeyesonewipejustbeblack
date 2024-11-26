cc8b4eda4d8db72896a78d56167da4cac303baa0
huangxinyu <1492318526@qq.com>
2024-11-26 11:26:22 +0800

    加载数据集

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -5,15 +5,13 @@ from PIL import Image
 import os
 import json
 import torchvision.transforms as T
-from pycocotools import mask
-from torchvision import datasets
 
 class CocoDataset(Dataset):
     def __init__(self, annotation_file, img_dir, transforms=None):
-        with open(annotation_file, 'r') as f:
+        with open(annotation_file, 'r') as f:# COCO 格式的标注文件（JSON 格式）
             self.data = json.load(f)
         
-        self.img_dir = img_dir
+        self.img_dir = img_dir # 图片目录
         self.transforms = transforms
         self.img_ids = [item['id'] for item in self.data['images']]
 
@@ -22,32 +20,32 @@ class CocoDataset(Dataset):
     
     def __getitem__(self, idx):
         img_info = self.data['images'][idx]
-        img_path = os.path.join(self.img_dir, img_info['file_name'])
+        img_path = os.path.join(self.img_dir, img_info['file_name']) # 图片的完整路径
         img = Image.open(img_path).convert("RGB")
 
-        # 获取目标框（bounding boxes）和标签（labels）
         target = {}
         target['boxes'] = []
         target['labels'] = []
+        target['area'] = []
 
-        for ann in self.data['annotations']:
-            if ann['image_id'] == img_info['id']:
-                # 如果图片中有目标（篡改的区域），则添加到目标框中
-                target['boxes'].append(ann['bbox'])
-                target['labels'].append(ann['category_id'])
+        for annotation in self.data['annotations']:
+            if annotation['image_id'] == img_info['id']:
+                target['boxes'].append(annotation['bbox'])
+                target['labels'].append(annotation['category_id'])
+                target['area'].append(annotation['area'])
         
         target['boxes'] = torch.as_tensor(target['boxes'], dtype=torch.float32)
         target['labels'] = torch.as_tensor(target['labels'], dtype=torch.int64)
+        target['area'] = torch.as_tensor(target['area'], dtype=torch.float32)
 
-        # 执行图像转换（可选）
         if self.transforms:
             img, target = self.transforms(img, target)
 
         return img, target
-
-# 定义数据增强 TODO
+    
+# TODO
 def get_transform():
     return T.Compose([
         T.ToTensor(),
-        T.Resize((800, 800)),  # TODO 根据需要调整大小
+        T.Resize((800, 800)),
     ])
