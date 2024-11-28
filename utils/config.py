c76e93e94f4f2674df5ccf261d7c203e6c393f71
ZiBeiK <1761713549@qq.com>
2024-11-28 19:23:10 +0800

    Update config.py

--- utils/config.py
+++ utils/config.py
@@ -7,9 +7,24 @@ from pprint import pprint
 
 class Config:
     # data
-    voc_data_dir = (
-        r"C:\Users\louis\Downloads\VOC\VOCtrainval_06-Nov-2007\VOCdevkit\VOC2007"
-    )
+    # voc_data_dir = (
+    #     r"C:\Users\louis\Downloads\VOC\VOCtrainval_06-Nov-2007\VOCdevkit\VOC2007"
+    # )
+    TRAIN_LABEL = 'data/label_train.json'
+    TRAIN_IMG_DIR = 'data/image/train/'
+    TEST_IMG_DIR = 'data/image/val/'
+    TRAIN_ANN = 'data/annotations/train.json'
+    VAL_ANN = 'data/annotations/val.json'
+
+    TRAIN_RATIO = 0.8 # 训练集占比
+    
+    # 网络参数 TODO
+    NUM_CLASSES = 3
+    LEARNING_RATE = 0.005
+    BATCH_SIZE = 8
+    EPOCHS = 10
+    DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+    
     min_size = 600  # image resize
     max_size = 1000  # image resize
     num_workers = 8
