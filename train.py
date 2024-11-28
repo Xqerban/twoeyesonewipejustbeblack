725e1390d3f155ded8687c25049ec48b9b562608
ZiBeiK <1761713549@qq.com>
2024-11-28 20:02:24 +0800

    Update train.py

--- train.py
+++ train.py
@@ -33,7 +33,7 @@ def train():
     val_loader = DataLoader(val_dataset, batch_size=Config.BATCH_SIZE, shuffle=False, collate_fn=lambda x: tuple(zip(*x)))
 
     # TODO
-    model = FasterRCNNVGG16(n_fg_class=Config.NUM_CLASSES)
+    model = FasterRCNNVGG16(n_fg_class=Config.NUM_CLASSES, ratios=[0.5, 1, 2], anchor_scales=[8, 16, 32])
     model.to(Config.DEVICE)
 
     params = [p for p in model.parameters() if p.requires_grad]
